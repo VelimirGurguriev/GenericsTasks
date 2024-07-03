141dd304bfe0cf0b0e1cec046faaf02e2d5a56a9
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-07-03 19:55:49 +0300

    added create method and implementation

--- src/CreatorArrayGeneric/ArrayCreator.java
+++ src/CreatorArrayGeneric/ArrayCreator.java
@@ -1,9 +1,18 @@
 package CreatorArrayGeneric;
 
 public class ArrayCreator <T> {
-    
+    private T[] array;
+    private int length;
+
     public ArrayCreator() {
 
+    }
+
+    public T[] create(int length, T item) {
+        for (int i = 0; i < length; i++) {
+            this.array[i] = item;
+        }
+        return this.array;
     }
 
 }
