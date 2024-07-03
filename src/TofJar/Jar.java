b4ae58d0d853480e32f4464088851523dcbdc4a0
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-07-03 17:42:48 +0300

    changed elements to type deque

--- src/TofJar/Jar.java
+++ src/TofJar/Jar.java
@@ -1,7 +1,14 @@
 package TofJar;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Jar <T> {
-    private T[] elements;
+    private Deque<T> elements;
+
+    public Jar() {
+        this.elements = new ArrayDeque<>();
+    }
 
     public void add(T element) {
 
