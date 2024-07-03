356f5ab5a7aa528d77e65fa03beb4f342a714fb7
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-07-03 18:00:36 +0300

    implemented add and remove methods

--- src/TofJar/Jar.java
+++ src/TofJar/Jar.java
@@ -4,13 +4,17 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class Jar <T> {
-    private Deque<T> elements;
+    private final Deque<T> elements;
 
     public Jar() {
         this.elements = new ArrayDeque<>();
     }
 
     public void add(T element) {
+        this.elements.push(element);
+    }
 
+    public T remove() {
+        return this.elements.pop();
     }
 }
