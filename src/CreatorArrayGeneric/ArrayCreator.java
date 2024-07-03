fff5add595ed88540af9c780ddcf26cb867a78bf
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-07-03 20:51:15 +0300

    rewrote logic of generic create methods, now they works as intentended

--- src/CreatorArrayGeneric/ArrayCreator.java
+++ src/CreatorArrayGeneric/ArrayCreator.java
@@ -5,7 +5,7 @@ import java.lang.reflect.Array;
 public class ArrayCreator <T> {
 
     public static <T> T[] create(int length, T item) {
-        T[] array = (T[]) new Object[length];
+        T[] array = (T[]) Array.newInstance(item.getClass(), length);
         for (int i = 0; i < length; i++) {
             array[i] = item;
         }
@@ -18,6 +18,18 @@ public class ArrayCreator <T> {
             array[i] = item;
         }
         return array;
+    }
+
+    public static void main(String[] args) {
+        Integer[] intArray = ArrayCreator.create(2, 123);
+        for (Integer i : intArray) {
+            System.out.println(i);
+        }
+
+        String[] strArray = ArrayCreator.create(String.class, 2, "abc");
+        for (String s : strArray) {
+            System.out.println(s);
+        }
     }
 
 }
