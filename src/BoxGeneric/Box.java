bbaf07c57b2fd0712041f43563436fab697034d6
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-07-04 19:50:48 +0300

    added generic item variable in Box class, overridden toString

--- src/BoxGeneric/Box.java
+++ src/BoxGeneric/Box.java
@@ -1,4 +1,14 @@
 package BoxGeneric;
 
 public class Box <T> {
+    private T item;
+
+    public Box(T item) {
+        this.item = item;
+    }
+    
+    @Override
+    public String toString() {
+        return item.getClass().getName() + ": " + item;
+    }
 }
--- src/BoxGeneric/Main.java
+++ src/BoxGeneric/Main.java
@@ -2,6 +2,6 @@ package BoxGeneric;
 
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
