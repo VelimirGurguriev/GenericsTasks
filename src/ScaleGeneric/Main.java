53b3ff7a2a102f31b73b1af180ee41cc5a89421c
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-07-04 18:22:38 +0300

    tested generic class Scale with Integers

--- src/ScaleGeneric/Main.java
+++ src/ScaleGeneric/Main.java
@@ -2,6 +2,8 @@ package ScaleGeneric;
 
 public class Main {
     public static void main(String[] args) {
-        
+        // testing the generic scale class with integers
+        Scale<Integer> scale = new Scale<>(1,2);
+        System.out.println(scale.getHeavier());
     }
 }
