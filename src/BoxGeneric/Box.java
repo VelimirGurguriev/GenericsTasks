c2e6419c1b8ffaaf1c11e0ced58051418746c495
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-07-04 20:28:31 +0300

    implemented swap method

--- src/BoxGeneric/Box.java
+++ src/BoxGeneric/Box.java
@@ -1,5 +1,7 @@
 package BoxGeneric;
 
+import java.util.List;
+
 public class Box <T> {
     private T item;
 
@@ -10,5 +12,12 @@ public class Box <T> {
     @Override
     public String toString() {
         return item.getClass().getName() + ": " + item;
+    }
+
+    // Generic swap method
+    public static <T> void swap(List<T> list, int firstIndex, int secondIndex) {
+        T tempItem = list.get(firstIndex);
+        list.set(firstIndex, list.get(secondIndex));
+        list.set(secondIndex, tempItem);
     }
 }
