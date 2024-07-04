7cb344e336cbe4ed5e8721380c4ff01374aa10d5
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-07-04 20:35:50 +0300

    rewrote Main method for GenericBox, added methods for different type of boxes.

--- src/BoxGeneric/Main.java
+++ src/BoxGeneric/Main.java
@@ -9,19 +9,31 @@ public class Main {
         int numberOfObjects = Integer.parseInt(scanner.nextLine());
 
         // generic box, class type String
+        boxOfStrings(numberOfObjects, scanner);
+
+        // generic box, class type Integer
+        boxOfIntegers(numberOfObjects, scanner);
+
+        
+
+        scanner.close();
+    }
+
+    private static void boxOfStrings(int numberOfObjects, Scanner scanner) {
+        System.out.println("Enter " + numberOfObjects + " String elements:");
         for (int i = 0; i < numberOfObjects; i++) {
             String userInput = scanner.nextLine();
             Box<String> stringBox = new Box<>(userInput);
             System.out.println(stringBox);
         }
+    }
 
-        // generic box, class type Integer
+    private static void boxOfIntegers(int numberOfObjects, Scanner scanner) {
+        System.out.println("Enter " + numberOfObjects + " Integer elements:");
         for (int i = 0; i < numberOfObjects; i++) {
             int userInput = Integer.parseInt(scanner.nextLine());
             Box<Integer> integerBox = new Box<>(userInput);
             System.out.println(integerBox);
         }
-
-        scanner.close();
     }
 }
