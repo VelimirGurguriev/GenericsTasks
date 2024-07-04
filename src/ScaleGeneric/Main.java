c735b87f123dec5babdd530cc7605906cd17adc3
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-07-04 18:25:45 +0300

    tested the generic Scale class with strings

--- src/ScaleGeneric/Main.java
+++ src/ScaleGeneric/Main.java
@@ -9,5 +9,9 @@ public class Main {
         // testing the generic scale with two equal double numbers
         Scale<Double> doubleScale = new Scale<>(3.14,3.14);
         System.out.println(doubleScale.getHeavier());
+
+        // testing the generic scale with strings
+        Scale<String> stringScale = new Scale<>("abc", "xyz");
+        System.out.println(stringScale.getHeavier());
     }
 }
