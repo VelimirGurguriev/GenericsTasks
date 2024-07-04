8869fe72a44d72e356bb7f5be74d29b9c7588b80
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-07-04 18:20:08 +0300

    implemented getHeavier method using compareTo

--- src/ScaleGeneric/Scale.java
+++ src/ScaleGeneric/Scale.java
@@ -8,4 +8,15 @@ public class Scale <T extends Comparable<T>> {
         this.left = left;
         this.right = right;
     }
+
+    public T getHeavier() {
+        int compare = this.left.compareTo(this.right);
+        if (compare >0) {
+            return this.left;
+        } else if (compare < 0) {
+            return this.right;
+        } else {
+            return null;
+        }
+    }
 }
