6a57c3a44110d41f0b85b806f13da6303373d6b4
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-07-04 18:03:36 +0300

    added left and right generic variables and constructor

--- src/ScaleGeneric/Scale.java
+++ src/ScaleGeneric/Scale.java
@@ -1,4 +1,11 @@
 package ScaleGeneric;
 
 public class Scale <T extends Comparable<T>> {
+    private T left;
+    private T right;
+
+    Scale(T left, T right) {
+        this.left = left;
+        this.right = right;
+    }
 }
