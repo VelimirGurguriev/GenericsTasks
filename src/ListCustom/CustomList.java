65d5ecf84748a1bcd9f214743b88ff7e362ae29d
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-07-05 14:05:42 +0300

    fixed the constructor, removed parameters, becuause there is no need of them when instantiating the class

--- src/ListCustom/CustomList.java
+++ src/ListCustom/CustomList.java
@@ -8,8 +8,12 @@ import java.util.List;
 public class CustomList <T extends Comparable<T>> implements Comparable<Box<T>> {
     private final List<T> elements;
 
-    public CustomList(T element) {
+    public CustomList() {
         this.elements = new ArrayList<>();
+    }
+
+    public void add(T element) {
+        
     }
 
     @Override
