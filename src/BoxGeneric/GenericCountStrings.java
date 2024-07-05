750e935b566532889cfae8cd9bba106650a21258
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-07-05 13:48:00 +0300

    tested generic count method with strings

--- src/BoxGeneric/GenericCountStrings.java
+++ src/BoxGeneric/GenericCountStrings.java
@@ -1,7 +1,27 @@
 package BoxGeneric;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class GenericCountStrings {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int numberOfObjects = Integer.parseInt(scanner.nextLine());
+        List<Box<String>> stringBoxList = new ArrayList<>();
+
+        for (int i = 0; i < numberOfObjects; i++) {
+            String userInput = scanner.nextLine();
+            Box<String> stringBox = new Box<>(userInput);
+            stringBoxList.add(stringBox);
+        }
+
+        String compareElement = scanner.nextLine();
+        Box<String> stringCompareBox = new Box<>(compareElement);
+
+        System.out.println(Box.countGreater(stringBoxList, stringCompareBox));
+
+        scanner.close();
     }
 }
